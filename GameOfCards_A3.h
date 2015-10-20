00dc5b7bfa0795728367dd545871d1dca8a1375a
Alicia Rodriguez <aliciarodriguez@alicias-mbp.att.net>
2015-10-19 20:20:43 -0400

    Initial commit

---
+++ GameOfCards_A3.h
@@ -0,0 +1,13 @@
+//
+//  GameOfCards_A3.h
+//  
+//
+//  Created by Alicia Rodriguez on 10/19/15.
+//
+
+#ifndef GameOfCards_A3_h
+#define GameOfCards_A3_h
+
+#include <stdio.h>
+
+#endif /* GameOfCards_A3_h */
---
+++ GameOfCards_A3_main.c
@@ -0,0 +1,8 @@
+//
+//  GameOfCards_A3.c
+//  
+//
+//  Created by Alicia Rodriguez on 10/19/15.
+//
+
+#include "GameOfCards_A3.h"
