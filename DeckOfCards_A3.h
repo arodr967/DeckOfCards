6b5f41beebbcbeb61a5fe74252e9250b7353d82f
Alicia Rodriguez <aliciarodriguez@alicias-macbook-pro.local>
2015-10-19 21:07:50 -0400

    Added constants.

--- DeckOfCards_A3.h
+++ DeckOfCards_A3.h
@@ -9,5 +9,8 @@
 #define DeckOfCards_A3_h
 
 #include <stdio.h>
+#define PLAYER_LIMIT 13 /* The limit of players */
+#define CARD_LIMIT 13 /* The limit of cards */
+#define MIN 2 /* Minumum number of players and cards */
 
 #endif /* DeckOfCards_A3_h */
